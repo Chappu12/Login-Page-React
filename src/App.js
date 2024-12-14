67f0c9b92058ecf74ea547feb36b682fbbfb0d98
prathamesh <prathamesh69bhavsar@gmail.com>
2024-12-14 12:42:34 +0530

    added 2 divs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <div className='main'>
+        <div className='first'>
+          <h1>React App</h1>
+        </div>
+        <div className='second'>
+          <h1>React App</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
